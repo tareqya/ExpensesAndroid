990804d2b8de796ffd9655fa484de9468faa71f6
Tareq Yassin <tareq.ya95@gmail.com>
2023-12-01 12:53:04 +0200

    finish screens design

---
+++ app/src/main/java/com/example/expensesapp/main/UpdateFragment.java
@@ -0,0 +1,26 @@
+package com.example.expensesapp.main;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.expensesapp.R;
+
+
+public class UpdateFragment extends Fragment {
+
+    public UpdateFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_update, container, false);
+    }
+}
